501fa1fb0b21e703a82b7edaa7c71150b53d5b47
k-agau <ksilvergold18@gmail.com>
2021-08-06 01:19:11 -0400

    Smart contract updates
    Almost done with the smart contract. Will do testing tomorrow and then add mock payment streams, etc to complete the implementation of this part

--- src/Backend/Contracts/PaymentFactory.sol
+++ src/Backend/Contracts/PaymentFactory.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.7.1;
 
 /** TO DO:
  * Check states
- * Check the logic behind withdrawing funds after certain amount of time
- * Implement "loan" idea. Also flesh out and discuss it more. Implement a function where creator can only increase his holdings
+ * Withdrawing funds
+ * Mediation
+ * Implement loan idea. Also flesh out and discuss it more. Implement a function where creator can only increase his holdings
  *              Hard to do w/o someone (the seller) being punished
  * Implement the streampay
  * Clean up code + comments
- * Fix the stake ether function. Remove and put elsewhere
- * Put more withdrawal functions in case
  * Add a stake for the worker to make up for gas fees if bails on job? Does this act as more of a deterrent though?
  * Begin testing
+ * Add require statements + messages w/ require statements
  **/ 
 
 contract PaymentFactory {
@@ -29,32 +29,31 @@ contract PaymentFactory {
     
     struct Job {
         address creator;
-        uint downPayment;
-        uint incrementPay;
-        uint timeBeforeStakeRemoved;
+        uint amount;
         State state;
         bool creatorSigned;
         bool applicantSigned;
+        bool workSubmitted;
     }
     
     // Mappings
-    mapping(uint => address[]) jobToApplicants;
-    mapping(uint => address) finalApplicant;
+    mapping(uint => address[]) public jobToApplicants;
+    mapping(uint => address) public finalApplicant;
     
     // Arrays
-    Job[] jobs;
+    Job[] public jobs;
     
     // Events
-    event JobCreated(address creator, uint downPayment, uint incrementPay, uint timeBeforeStakeRemoved, uint jobID);
+    event JobCreated(address creator, uint initAmount, uint jobID);
     event DownpaymentChanged();
     event ApplicantApplied(address applicant, uint jobID);
     event ApplicantChosen(address applicant, uint jobID);
-    event ApplicantSigned(uint jobID, address applicant);
-    event CreatorSigned(uint jobID, address creator);
+    event ApplicantSigned(address applicant, uint jobID);
+    event CreatorSigned(address creator, uint jobID);
     event JobCompleted(uint jobID);
     event FinalSign(address creator, address applicant, uint jobID);
-    event StakeRemoved(address creator, uint amount, uint jobID);
-    
+    event FinalResult(address creator, address applicant, uint jobID, bool law);
+
     constructor() {}
     
     // Modifiers
@@ -63,89 +62,56 @@ contract PaymentFactory {
      * @notice Confirms if the sender is the right address
      **/ 
     modifier auth(address src) {
-        require(src == msg.sender);
+        require(src == msg.sender, "Not authorized");
         _;
     }
     
     /**
      * @notice Confirms if the job is in the correct state
      **/ 
-    modifier isState(State _state, uint jobID) {
-        require(jobs[jobID].state == _state);
+    modifier inState(State _state, uint jobID) {
+        require(jobs[jobID].state == _state, "Incorrect state");
         _;
     }
-    
-    
-    // Private functions
-    
-    function _signalExternal() private {
         
-    }
-    
-    function _createPaymentStream() private {
-        
-    }
-    
-    
     // Public functions
     
     /**
-     * @notice Ether is staked by the creator for the job.
-     * @dev This function is most likely not logically written. Staking should be done inside the signing method
-     **/ 
-    function stakeEther(uint jobID) public payable auth(jobs[jobID].creator) {
-        require(msg.value >= jobs[jobID].downPayment);
-    }
-    
-    /**
      * @notice A new job is created with the terms set by the job creator
-     * @dev Some variables may be unneeded, such as the creator signing var
-     * @dev look into incrementPay
-     * @dev Look into timeBeforeStakeRemoved
-     * @param _downPayment The initial downpayment that is staked by the job creator
-     * @param _incrementPay The initial increment per event (more on this... probably an oracle is the right way to go about this param. Could also standardize it)
-     * @param _timeBeforeStakeRemoved The time before the job creator can remove his stake after the job is submitted
-     **/ 
-    function createJob(uint _downPayment, uint _incrementPay, uint _timeBeforeStakeRemoved) public {
+     * @dev Look into mediation and whether or not names/descs should be held in the backend
+     * @param _initAmount The initial amount that is staked by the job creator
+     **/
+    function createJob(uint _initAmount) public {
         jobs.push(Job({
             creator: msg.sender,
-            downPayment: _downPayment,
-            incrementPay: _incrementPay,
-            timeBeforeStakeRemoved: _timeBeforeStakeRemoved + block.timestamp,
+            amount: _initAmount,
             state: State.Open,
             creatorSigned: false,
-            applicantSigned: false
+            applicantSigned: false,
+            workSubmitted: false
         }));
         uint jobID = jobs.length - 1;
-        emit JobCreated(msg.sender, _downPayment, _incrementPay, _timeBeforeStakeRemoved, jobID);
+        emit JobCreated(msg.sender, _initAmount, jobID);
     }
     
     /**
-     * @notice The job creator can configure the down payment before finalizing his choice of applicant
-     * @dev downpayment will be confirmed during the signing process
-     * @param newDownPayment The new down payment
-     * @param jobID The ID of a specific job
-     **/ 
-    function configureJobDownPayment(uint newDownPayment, uint jobID) public auth(jobs[jobID].creator) isState(State.Open, jobID) {
-        require(newDownPayment > 0);
-        jobs[jobID].downPayment = newDownPayment;
+     * notice The job creator can configure the down payment before finalizing his choice of applicant
+     * dev downpayment will be confirmed during the signing process
+     * param newAmount The new down payment
+     * param jobID The ID of a specific job
+     **/
+    function configureAmount(uint newAmount, uint jobID) public auth(jobs[jobID].creator) inState(State.Open, jobID) {
+        require(newAmount > 0);
+        jobs[jobID].amount = newAmount;
         // Emit events
     }
-    
-    function configureJobIncrementPay(uint newIncrement, uint jobID) public auth(jobs[jobID].creator) isState(State.Open, jobID) {
-        require(newIncrement > 0);
-    }
-    
-    function configureJobTimeBeforeStakeRemoved(uint newTimeBeforeCanRemove, uint jobID) public auth(jobs[jobID].creator) isState(State.Open, jobID) {
-        require(newTimeBeforeCanRemove > 0);
-    }
-    
+
     /**
      * @notice Applicants apply for a specific job
      * @dev Can flesh out a little bit more, but this is probably ok for now
      * @param jobID The ID of a specific job
      **/ 
-    function applyForJob(uint jobID) public {
+    function applyForJob(uint jobID) public inState(State.Open, jobID) {
         jobToApplicants[jobID].push(msg.sender);
         emit ApplicantApplied(msg.sender, jobID);
     }
@@ -157,17 +123,20 @@ contract PaymentFactory {
      * @param chosenApplicant The applicant who is accepted
      * @param jobID The ID of a specific job
      **/ 
-    function chooseApplicant(address chosenApplicant, uint jobID) public auth(jobs[jobID].creator) {
+    function chooseApplicant(address chosenApplicant, uint jobID) public auth(jobs[jobID].creator) inState(State.Open, jobID) {
         finalApplicant[jobID] = chosenApplicant;
+        jobs[jobID].state = State.Signing;
+        emit ApplicantChosen(chosenApplicant, jobID);
     }
     
     /**
      * @notice The applicant signs off and agrees to do a job. The applicant can still withdraw from the commitment before the creator also signs off
-     * @dev Make sure states make sense. Add a function to withdraw
+     * @dev Add init stake to prevent bailing? May be a deterrent...
      * @param jobID The ID of a specific job
      **/ 
-    function initApplicantSign(uint jobID) public {
-        
+    function initApplicantSign(uint jobID) public auth(finalApplicant[jobID]) inState(State.Signing, jobID) {
+        jobs[jobID].applicantSigned = true;
+        emit ApplicantSigned(msg.sender, jobID);
     }
     
     /**
@@ -175,8 +144,13 @@ contract PaymentFactory {
      * @dev Check the states
      * @param jobID The ID of a specific job
      **/ 
-    function initCreatorSign(uint jobID) public auth(jobs[jobID].creator) {
-        
+    function initCreatorSign(uint jobID) public payable auth(jobs[jobID].creator) inState(State.Signing, jobID) {
+        uint val = msg.value;
+        require(jobs[jobID].amount >= val);
+        jobs[jobID].state = State.Signed;
+        jobs[jobID].amount = val;
+        jobs[jobID].creatorSigned = true;
+        emit CreatorSigned(msg.sender, jobID);
     }
     
     /**
@@ -186,33 +160,26 @@ contract PaymentFactory {
      * @dev Implement states and refusals
      * @param jobID The ID of a specific job
      **/ 
-    function submitWork(uint jobID) public auth(finalApplicant[jobID]){
-        
+    function submitWork(uint jobID) public auth(finalApplicant[jobID]) inState(State.Signed, jobID) {
+        jobs[jobID].workSubmitted = true; 
     }
     
     /**
      * @notice The job creators final sign off. Signals event tracking and the payment stream
-     * @dev Refusals and states
+     * @dev Include ability to refuse? Who would pay gas to refuse? Just don't pay...
+     * @dev In response to above comment: could be where mediation comes in. Mediator checks work, then decides who receives stake?
+     * @dev Would need to emphasize a high initAmount in that case
+     * @param law Dictates whether the work is accepted or rejected. True to accept, false to reject
      * @param jobID The ID of a specific job
      **/
-    function finalSign(uint jobID) public auth(jobs[jobID].creator) isState(State.Completed, jobID) {
-        
-    }
-    
-    /**
-     * @notice The job creator can remove his stake after the allotted time
-     * @dev Need to discuss this idea more. Worker should be guaranteed some pay + extra as an investment
-     * @param jobID The ID of a specific job
-     **/ 
-    function removeStake(uint jobID) public payable auth(jobs[jobID].creator) {
-        if(block.timestamp > jobs[jobID].timeBeforeStakeRemoved) {
-            // Need to make sure stream cancels
-            // Also need to implement remaining funds tracker
-            // Need require statement for above rather than an if
-            // Need a require statement to see if has the correct amount
-            (payable(jobs[jobID].creator)).transfer(jobs[jobID].downPayment);
-            emit StakeRemoved(msg.sender, jobs[jobID].downPayment, jobID);
+    function finalSign(bool law, uint jobID) public auth(jobs[jobID].creator) {
+        require(jobs[jobID].workSubmitted == true);
+        jobs[jobID].state = State.Closed;
+        if(law) {
+            // Begin payment stream + events
+        } else if(!law) {
+            // Mediation? Receive stake back after mediation?
         }
+        emit FinalResult(msg.sender, finalApplicant[jobID], jobID, law);
     }
-    
 }
