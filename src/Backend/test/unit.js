f6f5185233d5e9a7d8394a4420ed2489e20b5428
k-agau <ksilvergold18@gmail.com>
2021-08-07 11:48:11 -0400

    Updates

--- src/Backend/test/unit.js
+++ src/Backend/test/unit.js
@@ -14,18 +14,46 @@ describe("Payment Factory", function () {
         PaymentFactory = await ethers.getContractFactory("PaymentFactory");
         [buyer1, buyer2, seller1, seller2, seller3] = await ethers.getSigners();
         factory = await PaymentFactory.deploy();
-    });
-    
+      });
+
     describe("Deployment", function () {
         it("Deployment results in nothing for now", async function () {
         })
     });
-
+    
     describe("Create job", function () {
         it("New job should have jobID 0", async function () {
-            let _batches = ["0", "1", "2"];
+            //let _batches = ["0", "1", "2"];
             //const job1 = await factory.connect(buyer1).createJob(1000, _batches, 10);
-            //expect(factory.jobs[0]).to.equal(job1);
+            //expect(await factory.jobs(0)).to.equal(job1);
+        });
+
+        it("Creator should be buyer1", async function () {
+            expect(await factory.jobCreator(0)).to.equal(buyer1);
+        });
+
+        it("Amount should be 1000", async function () {
+            expect(await factory.jobAmount(0)).to.equal(1000);
+        });
+
+        it("Creator signed should be false", async function () {
+            expect(await factory.jobCreatorSignature(0)).to.equal(false);
+        });
+
+        it("Applicant signed should be false", async function () {
+            expect(await factory.jobApplicantSignature(0)).to.equal(false);
+        });
+
+        it("Work submitted should be false", async function () {
+            expect(await factory.jobWorkSubmitted(0)).to.equal(false);
+        });
+
+        it("", async function () {
+            //expect(await factory.jobAmount(0)).to.equal(1000);
+        });
+
+        it("State should be open", async function () {
+            expect(await factory.jobState(0)).to.equal(factory.State.Open);
         });
     });
 
